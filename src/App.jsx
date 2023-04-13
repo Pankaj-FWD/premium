c13f6bd0732438f842bd6196dc2a14979922f162
pankaj <panpal@publicisgroupe.net>
2023-04-13 15:09:08 +0530

    react file final1

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ const App = () => {
 			<Navbar />
 			<Routes>
 				<Route index element={<Home />} />
+				<Route path="premium" element={<Home />} />
 				<Route path="about" element={<About />} />
 				<Route path="contact" element={<Contact />} />
 				<Route path="gallery" element={<Gallery />} />
 				<Route path="plans" element={<Plans />} />
 				<Route path="trainers" element={<Trainers />} />
-				{/* <Route path="*" element={<NotFound />} /> */}
-				<Route path="*" element={<Home />} />
-
+				<Route path="*" element={<NotFound />} />
+				
 			</Routes>
 			<Footer />
 		</BrowserRouter>
