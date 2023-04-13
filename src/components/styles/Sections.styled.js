e2ffc6a6404ead172524eee65af08aadea48540c
pankaj <panpal@publicisgroupe.net>
2023-04-13 14:40:57 +0530

    react file

--- src/components/styles/Sections.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Section = styled.section``;
-
-export const SectionCards = styled.div`
-  @media all and (min-width: ${({ theme }) => theme.breakpoints.md}) {
-    margin-top: 2.25rem;
-  }
-`;
---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./contact.css";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
+
+const Contact = () => {
+	return (
+		<>
+			<Header title="Get In Touch" image={HeaderImage}>
+			"If you have any questions, comments, or concerns, please don't hesitate to reach out to us.
+			 You can fill out the contact form below or call us . We're always happy to
+			  hear from our members and are here to help in any way we can."
+			</Header>
+
+			<section>
+    
+   
+    
+    <div class="container">
+      <div class="row">
+        
+        <div class="contact-info">
+          <div class="contact-info-item">
+            <div class="contact-info-icon">
+              <i class="fas fa-home"></i>
+            </div>
+            
+            <div class="contact-info-content">
+              <h4>Address</h4>
+              <p>Near old petrole pump,<br/> CSB Road, Saurikh, <br/>209728</p>
+            </div>
+          </div>
+          
+          <div class="contact-info-item">
+            <div class="contact-info-icon">
+              <i class="fas fa-phone"></i>
+            </div>
+            
+            <div class="contact-info-content">
+              <h4>Phone</h4>
+              <p>+91-7317826455</p>
+            </div>
+          </div>
+          
+          <div class="contact-info-item">
+            <div class="contact-info-icon">
+              <i class="fas fa-envelope"></i>
+            </div>
+            
+            <div class="contact-info-content">
+              <h4>Email</h4>
+             <p>premium_fitness@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="contact-form">
+          <form action="" id="contact-form">
+            <h2>Send Message</h2>
+            <div class="input-box">
+              <input type="text" required="true" name=""/>
+              <span>Full Name</span>
+            </div>
+            
+            <div class="input-box">
+              <input type="email" required="true" name=""/>
+              <span>Email</span>
+            </div>
+            
+            <div class="input-box">
+              <textarea required="true" name=""></textarea>
+              <span>Type your Message...</span>
+            </div>
+            
+            <div class="input-box">
+              <input type="submit" value="Send" name=""/>
+            </div>
+          </form>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+		</>
+	);
+};
+
+export default Contact;
