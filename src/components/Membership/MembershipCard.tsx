11836ace13239d6156e5fe43f0cea8fa7e651a46
Pankaj Pal <panpal@publicisgroupe.net>
2023-04-13 10:36:42 +0530

    first

---
+++ src/components/Membership/MembershipCard.tsx
@@ -0,0 +1,54 @@
+import {
+  MembershipPrice,
+  MembershipStyledCard,
+  MembershipTier,
+  MembershipCardPer,
+  MembershipIncludesTitle,
+  MembershipList,
+  MembershipIncluded,
+  MembershipNotIncluded,
+  MembershipCardCTA,
+  MembershipListContainer,
+  PopularPricing,
+} from '@/components/styles/MembershipCard.styled';
+
+const MembershipCard = ({
+  tier,
+  price,
+  membershipIncludes,
+  primier,
+  anuallyPrice,
+  annualyFunction,
+}) => {
+  return (
+    <MembershipStyledCard>
+      {primier && <PopularPricing>popular pricing</PopularPricing>}
+      <MembershipTier>{tier}</MembershipTier>
+      <MembershipPrice>
+        ${annualyFunction ? anuallyPrice : price}
+        <MembershipCardPer>
+          / {annualyFunction ? ' year' : ' mo'}
+        </MembershipCardPer>
+      </MembershipPrice>
+      <MembershipIncludesTitle>What's included</MembershipIncludesTitle>
+      <MembershipListContainer>
+        {membershipIncludes.map((include) => {
+          return (
+            <MembershipList key={include.id}>
+              {include.included ? (
+                <MembershipIncluded />
+              ) : (
+                <MembershipNotIncluded />
+              )}
+              {include.membershipIncludes}
+            </MembershipList>
+          );
+        })}
+      </MembershipListContainer>
+
+      <MembershipCardCTA primier={primier}>start training</MembershipCardCTA>
+    </MembershipStyledCard>
+  );
+};
+
+export default MembershipCard;
