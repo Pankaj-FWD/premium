955389fbddcd342361b8f143b6a5fedc1ac48942
pankaj <panpal@publicisgroupe.net>
2023-08-19 14:44:45 +0530

    initial commit for premiumfit main web page

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./contact.css";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg";
+
+const Contact = () => {
+	return (
+		<>
+			<Header title="Get In Touch" image={HeaderImage}>
+			"If you have any questions, comments, or concerns, please don't hesitate to reach out to us.
+			 You can fill out the contact form below or call us . We're always happy to
+			  hear from our members and are here to help in any way we can."
+			</Header>
+
+			<section>
+    
+   
+    
+    <div class="container">
+      <div class="row">
+        
+        <div class="contact-info">
+          <div class="contact-info-item">
+            <div class="contact-info-icon">
+              <i class="fas fa-home"></i>
+            </div>
+            
+            <div class="contact-info-content">
+              <h4>Address</h4>
+              <p>C.S.B Road,<br/>Gandhi Nagar Saurikh, <br/>209728</p>
+            </div>
+          </div>
+          
+          <div class="contact-info-item">
+            <div class="contact-info-icon">
+              <i class="fas fa-phone"></i>
+            </div>
+            
+            <div class="contact-info-content">
+              <h4>Phone</h4>
+              <p>+91-7317826455</p>
+            </div>
+          </div>
+          
+          <div class="contact-info-item">
+            <div class="contact-info-icon">
+              <i class="fas fa-envelope"></i>
+            </div>
+            
+            <div class="contact-info-content">
+              <h4>Email</h4>
+             <p>contact@premiumfitness.co.in</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="contact-form">
+          <form action="" id="contact-form">
+            <h2>Send Message</h2>
+            <div class="input-box">
+              <input type="text" required="true" name=""/>
+              <span>Full Name</span>
+            </div>
+            
+            <div class="input-box">
+              <input type="email" required="true" name=""/>
+              <span>Email</span>
+            </div>
+            
+            <div class="input-box">
+              <textarea required="true" name=""></textarea>
+              <span>Type your Message...</span>
+            </div>
+            
+            <div class="input-box">
+              <input type="submit" value="Send" name=""/>
+            </div>
+          </form>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+		</>
+	);
+};
+
+export default Contact;
