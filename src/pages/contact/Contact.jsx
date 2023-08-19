05253d726f06e4c73be30c5d38e1070e0e2a3735
pankaj <panpal@publicisgroupe.net>
2023-08-19 16:17:51 +0530

    push to main

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import "./contact.css";
 import Header from "../../components/Header";
 import HeaderImage from "../../images/header_bg_2.jpg";
-import { MdEmail } from "react-icons/md";
-import { BsMessenger } from "react-icons/bs";
-import { IoLogoWhatsapp } from "react-icons/io";
 
 const Contact = () => {
 	return (
